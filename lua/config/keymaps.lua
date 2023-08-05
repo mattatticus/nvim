28944994128d021506a20beef654928eaa492a6d
TomLance0 <lancetom62@gmail.com>
2023-08-05 21:18:58 +0530

    Some Changes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,17 +7,19 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.command = ""
 
--- Split Navigation
 keymap("n", "<leader>n", "<C-w>w")
-keymap("n", "<leader>mh", "<C-w>h")
-keymap("n", "<leader>mj", "<C-w>j")
-keymap("n", "<leader>mk", "<C-w>k")
-keymap("n", "<leader>ml", "<C-w>l")
+-- keymap("n", "<leader>mh", "<C-w>h")
+-- keymap("n", "<leader>mj", "<C-w>j")
+-- keymap("n", "<leader>mk", "<C-w>k")
+-- keymap("n", "<leader>ml", "<C-w>l")
 
 keymap("n", "<leader>u", "<Cmd> UndotreeToggle<CR>")
 keymap("n", "<leader>h", "<Cmd> Telescope find_files<CR>")
 keymap("n", "<leader>s", "<Cmd> NvimTreeToggle<CR>")
 
 keymap("n", "<leader>f", "<Cmd> Format<CR>")
+
+keymap("n", "<leader>a", "<Cmd> BufferlineCycleNext<CR>")
+keymap("n", "<leader>m", "<Cmd> BufferlineCyclePrev<CR>")
 
 keymap("t", "<esc>", "<C-\\><C-n>")
