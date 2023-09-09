80918418983bbcb5abef95df1d5bf45586672a09
TomLance0 <tomlance62@gmail.com>
2023-09-09 17:11:35 +0530

    Refined configuration.

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,103 +1,100 @@
 return {
 	"mhartington/formatter.nvim",
 	cmd = { "FormatWrite", "Format" },
-	config = function()
-		local util = require("formatter.util")
-		require("formatter").setup({
-			filetype = {
-				lua = {
-					function()
-						return {
-							exe = "stylua",
-							args = {
-								"--search-parent-directories",
-								"--stdin-filepath",
-								util.escape_path(util.get_current_buffer_file_path()),
-								"--",
-								"-",
-							},
-							stdin = true,
-						}
-					end,
-				},
-				haskell = {
-					function()
-						return {
-							exe = "fourmolu",
-							args = { "--no-cabal" },
-							stdin = true,
-						}
-					end,
-				},
-				javascript = {
-					function()
-						return {
-							exe = "js-beautify",
-							stdin = true,
-							try_node_modules = true,
-						}
-					end,
-				},
-				html = {
-					function()
-						return {
-							exe = "html-beautify",
-							stdin = 1,
-						}
-					end,
-				},
-				css = {
-					function()
-						return {
-							exe = "css-beautify",
-							stdin = 1,
-						}
-					end,
-				},
-				cpp = {
-					function()
-						return {
-							exe = "clang-format",
-							args = { "--style='{BasedOnStyle: GNU, IndentWidth: 4}'" },
-							stdin = true,
-						}
-					end,
-				},
-				c = {
-					function()
-						return {
-							exe = "clang-format",
-							args = { "--style='{BasedOnStyle: GNU, IndentWidth: 4}'" },
-							stdin = true,
-						}
-					end,
-				},
-				rust = {
-					function()
-						return {
-							exe = "rustfmt",
-							stdin = true,
-						}
-					end,
-				},
-				go = {
-					function()
-						return {
-							exe = "gofmt",
-							stdin = true,
-						}
-					end,
-				},
-				zig = {
-					function()
-						return {
-							exe = "zig",
-							args = { "fmt", "--stdin" },
-							stdin = true,
-						}
-					end,
-				},
+	opts = {
+		filetype = {
+			lua = {
+				function()
+					return {
+						exe = "stylua",
+						args = {
+							"--search-parent-directories",
+							"--stdin-filepath",
+							vim.fn.shellescape(vim.api.nvim_buf_get_name(0), true),
+							"--",
+							"-",
+						},
+						stdin = true,
+					}
+				end,
 			},
-		})
-	end,
+			haskell = {
+				function()
+					return {
+						exe = "fourmolu",
+						args = { "--no-cabal" },
+						stdin = true,
+					}
+				end,
+			},
+			javascript = {
+				function()
+					return {
+						exe = "js-beautify",
+						stdin = true,
+						try_node_modules = true,
+					}
+				end,
+			},
+			html = {
+				function()
+					return {
+						exe = "html-beautify",
+						stdin = 1,
+					}
+				end,
+			},
+			css = {
+				function()
+					return {
+						exe = "css-beautify",
+						stdin = 1,
+					}
+				end,
+			},
+			cpp = {
+				function()
+					return {
+						exe = "clang-format",
+						args = { "--style='{BasedOnStyle: GNU, IndentWidth: 4}'" },
+						stdin = true,
+					}
+				end,
+			},
+			c = {
+				function()
+					return {
+						exe = "clang-format",
+						args = { "--style='{BasedOnStyle: GNU, IndentWidth: 4}'" },
+						stdin = true,
+					}
+				end,
+			},
+			rust = {
+				function()
+					return {
+						exe = "rustfmt",
+						stdin = true,
+					}
+				end,
+			},
+			go = {
+				function()
+					return {
+						exe = "gofmt",
+						stdin = true,
+					}
+				end,
+			},
+			zig = {
+				function()
+					return {
+						exe = "zig",
+						args = { "fmt", "--stdin" },
+						stdin = true,
+					}
+				end,
+			},
+		},
+	},
 }
