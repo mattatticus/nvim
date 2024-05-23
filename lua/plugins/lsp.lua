de05417122a97d2240fb79dfd07a91f019e47d2c
fortom967 <tonytomcat01@gmail.com>
2024-05-23 21:55:56 +0530

    * Removed unnecessary comments

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,57 +2,8 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-		dependencies = {
-			-- "ray-x/lsp_signature.nvim",
-		},
 
 		config = function()
-			--[[ local signature = require "lsp_signature"
-
-		local cfg = {
-			debug = false,
-			log_path = "/tmp/lsp_signature.log",
-			verbose = false,
-			bind = true,
-			doc_lines = 12,
-			max_height = 15,
-			max_width = 80,
-			noice = false,
-			wrap = true,
-			floating_window = true,
-			floating_window_above_cur_line = true,
-			floating_window_off_x = 1,
-			floating_window_off_y = 0,
-			close_timeout = 1000,
-			fix_pos = true,
-			hint_enable = true,
-			hint_prefix = " ",
-			hint_scheme = "String",
-			hi_parameter = "LspSignatureActiveParameter",
-			handler_opts = {
-				border = "none",
-			},
-
-			always_trigger = false,
-
-			auto_close_after = nil,
-			extra_trigger_chars = {},
-			zindex = 1000,
-
-			padding = "  ",
-
-			transparency = nil,
-			shadow_blend = 36,
-			shadow_guibg = "Black",
-			timer_interval = 200,
-			toggle_key = nil,
-			toggle_key_flip_floatwin_setting = false,
-
-			select_signature_key = nil,
-			move_cursor_key = nil,
-		}
-		signature.setup(cfg) ]]
-
 			local on_attach = function(_, bufnr)
 				vim.api.nvim_set_option_value("omnifunc", "v:lua.vim.lsp.omnifunc", { buf = bufnr })
 				local bufopts = { noremap = true, silent = true, buffer = bufnr }
