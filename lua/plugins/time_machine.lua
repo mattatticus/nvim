dc61abbc923d06f85a39ab0ba83fef8811054f13
Matthew Atticus <matthewatticus4@proton.me>
2025-05-11 13:00:37 +0530

    New plugin: time-machine.nvim

---
+++ lua/plugins/time_machine.lua
@@ -0,0 +1,24 @@
+local M = {
+    "y3owk1n/time-machine.nvim",
+    event = { "LazyFile" },
+}
+
+M.opts = {
+    split_opts = {
+        split = "right",
+        width = 50,
+    }
+}
+
+M.keys = {
+    {
+        "<leader>u",
+        function()
+            require "time-machine".actions.toggle()
+        end,
+        mode = { "n", "v" },
+        desc = "Power the Time Machine!!",
+    },
+}
+
+return M
